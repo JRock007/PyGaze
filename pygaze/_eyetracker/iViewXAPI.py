0b53c928a603e5d9438f3115944864f8c5021fdf
JRock007 <github@jean-romain.com>
2019-07-08 21:46:54 +0200

    Fix wrong data type passde to CEvent

--- pygaze/_eyetracker/iViewXAPI.py
+++ pygaze/_eyetracker/iViewXAPI.py
@@ -83,7 +83,7 @@ calibrationData = CCalibration(5, 1, 0, 0, 1, 20, 239, 1, 15, b"")
 leftEye = CEye(0,0,0)
 rightEye = CEye(0,0,0)
 sampleData = CSample(0,leftEye,rightEye,0)
-eventData = CEvent('F', 'L', 0, 0, 0, 0, 0)
+eventData = CEvent(b'F', b'L', 0, 0, 0, 0, 0)
 accuracyData = CAccuracy(0,0,0,0)
 
 
